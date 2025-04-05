b753158a9c4a359be6d8d2df87aea756898bc0db
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-04-05 18:54:27 +0300

    json verisi basıldı

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { ScrollView, StyleSheet, View, Text } from 'react-native';
+import { ScrollView, StyleSheet, Text } from 'react-native';
 import { Table, Row, Rows } from 'react-native-table-component';
 
+import data from './assets/db.json';
+
 export default function App() {
-  const tableHead1 = Array.from({ length: 12 }, (_, i) => `S${i + 1}`);
-  const tableData1 = Array.from({ length: 4 }, (_, rowIndex) =>
-    Array.from({ length: 12 }, (_, colIndex) => `R${rowIndex + 1}C${colIndex + 1}`)
-  );
+  const tableHead1 = ['ID', 'İsim', 'Email'];
+  const tableData1 = data.users.map(user => [user.id, user.name, user.email]);
 
   const tableHead2 = Array.from({ length: 8 }, (_, i) => `S${i + 1}`);
   const tableData2 = Array.from({ length: 2 }, (_, rowIndex) =>
@@ -16,7 +16,7 @@ export default function App() {
   return (
     <ScrollView style={styles.container}>
 
-      <Text style={styles.title}>Tablo 1 </Text>
+      <Text style={styles.title}>Tablo 1 - Kullanıcılar</Text>
       <Table borderStyle={styles.border}>
         <Row data={tableHead1} style={styles.head} textStyle={styles.text} />
         <Rows data={tableData1} textStyle={styles.text} />
@@ -28,7 +28,7 @@ export default function App() {
         <Rows data={tableData2} textStyle={styles.text} />
       </Table>
 
-      <Text style={styles.title}>Tablo 3 </Text>
+      <Text style={styles.title}>Tablo 3</Text>
       <Table borderStyle={styles.border}>
         <Row data={tableHead2} style={styles.head} textStyle={styles.text} />
         <Rows data={tableData2} textStyle={styles.text} />
